ce5ef8e679c387f7dbfc266d77c09f5b0a3f2ec4
joe-gregory <josephedward2nd@gmail.com>
2022-11-09 17:53:11 -0800

    installed ejs and fixed files to work with it

--- DDapp.js
+++ DDapp.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 //express app 
-const ddapp = express();
+const DDapp = express();
+
+//register view engine
+DDapp.set('view engine', 'ejs');
 
 //listen for requests
-ddapp.listen(8080);
+DDapp.listen(8080);
 
-ddapp.get('/', (request, response) => {
-    response.sendFile('./views/index.html', {root: __dirname});
+DDapp.get('/', (request, response) => {
+    response.render('index',{title: 'Democracia Directa'});
+});
+
+DDapp.use((request, response) => {
+    response.status(404).render('404', {title: '404'});
 });
