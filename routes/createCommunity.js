4e2fcbd552b6fd0967757b56832cfe45db21e076
joe-gregory <josephedward2nd@gmail.com>
2022-11-14 15:52:45 -0800

    Aesthetic changes to forms &, most importantly, added form to create communities. Next, create logic to handle: - Saving new community in DB - Generate the internal home objects that will later be assigned to citizens  Changes to be committed: 	modified:   routes/createCommunity.js 	modified:   views/createCommunity.ejs

--- routes/createCommunity.js
+++ routes/createCommunity.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/createCommunity', (request, response) =>{
-    response.render('createCommunity');
+    response.render('createCommunity', request.user);
 })
 
 module.exports = router;
