e68ef2a8854c7b64097d8deccbe37f3e76f3fa5b
joe-gregory <josephedward2nd@gmail.com>
2022-11-14 21:46:36 -0800

    Communities can now be created through forms. The inner homes are now generated and also saved to DB and associated with their respective community. Next: dynamically add list of options of existing communities and their homes in dropdown when associating with a community.
     Changes to be committed:
    	modified:   public/styles.css
    	modified:   routes/createCommunity.js
    	modified:   views/createCommunity.ejs
    	modified:   views/mycommunity.ejs
    	new file:   views/partials/noCommunity.ejs

--- routes/createCommunity.js
+++ routes/createCommunity.js
@@ -4,16 +4,23 @@ const router = express.Router();
 const Community = require('../models/community');
 
 router.get('/createCommunity', (request, response) =>{
+    if(!request.isAuthenticated()) response.redirect('/login');
     response.render('createCommunity', request.user);
 })
 
-router.post('createCommunity', (request, response) => {
+router.post('/createCommunity', (request, response) => {
+    
     const community = new Community.Community({
         name:  request.body.communityName,
         communityAddress: request.body.communityAddress, 
     });
+    for(let i = request.body.communityStartingNumber; i <= request.body.communityEndingNumber; i++){
+        home = new Community.Home({innerNumber: i});
+        home.save();
+        community.innerHomes.push(new Community.Home({innerNumber: i}));
+    }
     community.save()
-        .then((result) => response.redirect('mycommunity'))
+        .then((result) => response.redirect('/mycommunity'))
         .catch((err) => response.send(err));
 });
 
