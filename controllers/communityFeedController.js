11501ff38071959fbf0bb67846bc59342f634938
joe-gregory <josephedward2nd@gmail.com>
2022-12-18 12:17:58 -0800

    updated communityFeedController function

--- controllers/communityFeedController.js
+++ controllers/communityFeedController.js
@@ -1,85 +1,22 @@
 const CommunityModels = require('../models/communityModels');
 const CitizenActionModels = require('../models/citizenActionsModels');
+const dbController = require('./_dbController');
 
 const getCommunityFeed = async (request, response) => {
     
     if (!request.user.residencies.length) return response.redirect('/mycommunity/nocommunity');
     
-    let user_communities = [];
+    response.locals.communities = [];
     for(let i = 0; i < request.user.residencies.length; i++){
-        user_communities.push(request.user.residencies[i].community);
+       let community = await dbController(request.user.residencies[i].community);
+       response.locals.communities.push(community);
     }
 
-    let mongoCommunities = await CommunityModels.Community.find({'_id':{$in: user_communities}});
-    response.locals.communities = []
-    for(let i = 0; i < mongoCommunities.length; i++){
-        response.locals.communities.push(mongoCommunities[i]);        
-        let proposals = await CitizenActionModels.Proposal.find({'_id':{$in: mongoCommunities[i].proposals}});
-        response.locals.communities[i].proposals = proposals;
-        console.log(response.locals.communities[i].proposals);
-    }
-
-    //votes = await Law.Vote.find({'_id' : { $in: response.locals.proposals[j].votes}});
-
-    /////////////////////////////////////////////////////////////////////////
-    /*
-    let community = await Community.Community.findById(request.user.residencies[0].community); 
-    if (community === null) {
-        response.redirect('/profile');
-        return;
-    }
-    response.locals.community = community; //Save the community's information on locals to render on page
-    
-    //if there are no proposals yet, redirect to /mycommunity directly
-    if (community.proposals.length === 0) return response.render('mycommunity');
-    
-    //Search for the array of proposal IDs stored in the community model
-    let proposals = await Law.Proposal.find({'_id' : { $in: community.proposals}});
-    
-    response.locals.proposals = proposals;
-
-    //I need to find the array of votes.citizens in each proposal inside proposals
-    //so I can check it against req.user.id to see if the current user
-    //already voted on this proposal[j]
-    //taking advantage of the fact that I am looping through the list of proposals, I am going to add
-    //Law text to proposals about deleting existing laws so I can display it in its unit feed
-    for (let j = 0; j < response.locals.proposals.length; j++) {
-        //obtain an array of votes that match the ID of the proposal.votes array 
-        votes = await Law.Vote.find({'_id' : { $in: response.locals.proposals[j].votes}});
-
-        //Search to see if any vote.citizen id match the current user's id
-        let matchingVote = votes.find(vote => vote.citizen == request.user.id);
-
-        if(matchingVote != undefined){
-            response.locals.proposals[j].alreadyVoted = true;
-            response.locals.proposals[j].votedValue = matchingVote.inFavor;
-        } else{
-            response.locals.proposals[j].alreadyVoted = false;
-        }
-        
-        //get proposal's authors full name
-        let citizen = await Community.Citizen.findById(response.locals.proposals[j].author);
-        response.locals.proposals[j].authorName = `${citizen.firstName} ${citizen.lastName} ${citizen.secondLastName}`;
-        
-        //get proposal's author house number
-        home = await Community.Home.findById(citizen.home);
-        response.locals.proposals[j].houseNumber = home.innerNumber;
-
-        //Taking advantage that I am looping through proposals to add law text to delete proposals
-        if(response.locals.proposals[j].type === 'delete') {
-            
-            let law = await Law.Law.findById(response.locals.proposals[j].law);
-            let lawText = law.law;
-            let lawNumber = community.laws.indexOf(response.locals.proposals[j].law);
-            if (lawNumber) lawNumber++;
-            response.locals.proposals[j].lawText = lawText;
-        }
-    }
     response.render('mycommunity');
-    */
+
 };
 
-//dealing with voting on feed proposals:
+//Votes on feed units:
 const postFeedVote = async (request, response) =>{
     //Check if user has voted on this proposal, if so, redirect to /profile as safety
     let proposal = await Law.Proposal.findById(request.params.proposalId);
